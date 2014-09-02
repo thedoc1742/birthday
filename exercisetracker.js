38e467bb4759a4e3402a5fc91eb4ec588a378412
unknown <richter@PICTOR.e-spirit.de>
2014-09-03 01:10:33 +0200

    changed global variable

--- exercisetracker.js
+++ exercisetracker.js
@@ -25,19 +25,18 @@ document.addEventListener("deviceready", function(){
 
 });
 
-
 var track_id = '';      // Name/ID of the exercise
 var watch_id = null;    // ID of the geolocation
-var tracking_data = []; // Array containing GPS position objects
+window.tracking_data = []; // Array containing GPS position objects
 
 $("#startTracking_start").live('click', function(){
-    
 	// Start tracking the User
     watch_id = navigator.geolocation.watchPosition(
     
     	// Success
         function(position){
-            tracking_data.push(position);
+            window.tracking_data.push(position);
+            console.log(window.tracking_data);
         },
         
         // Error
@@ -46,28 +45,28 @@ $("#startTracking_start").live('click', function(){
         },
         
         // Settings
-        { frequency: 3000, enableHighAccuracy: true });
+        { frequency: 3000, maximumAge: 3000, enableHighAccuracy: true });
     
     // Tidy up the UI
     track_id = $("#track_id").val();
     
     $("#track_id").hide();
     
-    $("#startTracking_status").html("Tracking workout: <strong>" + track_id + "</strong>");
+    $("#startTracking_status").html("Tracking workout: <strong>" + track_id + window.tracking_data + "</strong>");
 });
-
 
 $("#startTracking_stop").live('click', function(){
 	
 	// Stop tracking the user
-	navigator.geolocation.clearWatch(watch_id);
+  navigator.geolocation.clearWatch(watch_id);
 	
+  console.log(window.tracking_data);	
 	// Save the tracking data
-	window.localStorage.setItem(track_id, JSON.stringify(tracking_data));
+	window.localStorage.setItem(track_id, JSON.stringify(window.tracking_data));
 
-	// Reset watch_id and tracking_data 
-	var watch_id = null;
-	var tracking_data = null;
+	// Reset watch_id and window.tracking_data 
+	watch_id = null;
+	window.tracking_data = [];
 
 	// Tidy up the UI
 	$("#track_id").val("").show();
@@ -81,8 +80,25 @@ $("#home_clearstorage_button").live('click', function(){
 });
 
 $("#home_seedgps_button").live('click', function(){
-	window.localStorage.setItem('Sample block', '[{"timestamp":1335700802000,"coords":{"heading":null,"altitude":null,"longitude":170.33488333333335,"accuracy":0,"latitude":-45.87475166666666,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700803000,"coords":{"heading":null,"altitude":null,"longitude":170.33481666666665,"accuracy":0,"latitude":-45.87465,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700804000,"coords":{"heading":null,"altitude":null,"longitude":170.33426999999998,"accuracy":0,"latitude":-45.873708333333326,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700805000,"coords":{"heading":null,"altitude":null,"longitude":170.33318333333335,"accuracy":0,"latitude":-45.87178333333333,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700806000,"coords":{"heading":null,"altitude":null,"longitude":170.33416166666666,"accuracy":0,"latitude":-45.871478333333336,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700807000,"coords":{"heading":null,"altitude":null,"longitude":170.33526833333332,"accuracy":0,"latitude":-45.873394999999995,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700808000,"coords":{"heading":null,"altitude":null,"longitude":170.33427333333336,"accuracy":0,"latitude":-45.873711666666665,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700809000,"coords":{"heading":null,"altitude":null,"longitude":170.33488333333335,"accuracy":0,"latitude":-45.87475166666666,"speed":null,"altitudeAccuracy":null}}]');
 
+    watch_id = navigator.geolocation.watchPosition(
+    
+    	// Success
+        function(position){
+            window.tracking_data.push(position);
+            window.localStorage.setItem('Sample block', JSON.stringify(window.tracking_data));
+        },
+        
+        // Error
+        function(error){
+            console.log(error);
+        },
+        
+        // Settings
+        { frequency: 3000, maximumAge: 3000, enableHighAccuracy: true });
+  
+     window.tracking_data = [];
+	
 });
 
 // When the user views the history page
@@ -127,18 +143,14 @@ $('#track_info').live('pageshow', function(){
 	
 	// Turn the stringified GPS data back into a JS object
 	data = JSON.parse(data);
-  
-  alert(data);
 
 	// Calculate the total distance travelled
 	total_km = 0;
 
 	for(i = 0; i < data.length; i++){
-	    
 	    if(i == (data.length - 1)){
 	        break;
 	    }
-	    
 	    total_km += gps_distance(data[i].coords.latitude, data[i].coords.longitude, data[i+1].coords.latitude, data[i+1].coords.longitude);
 	}
 	
